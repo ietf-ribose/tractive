c03d80a2307e36e3b9117446ea3bfb3894a99054
hassanakbar <hassanakbar2@gmail.com>
2021-09-02 13:58:42 +0500

    formatting bug fixes and subtickets removed

--- lib/tractive/info.rb
+++ lib/tractive/info.rb
@@ -10,24 +10,24 @@ module Tractive
 
     def result_hash
       users = [
-        Ticket.distinct(:reporter).select_map(:reporter),
-        Ticket.distinct(:owner).select_map(:owner),
-        TicketChange.distinct(:author).select_map(:author),
-        TicketChange.distinct(:newvalue).where(field: "reporter").select_map(:newvalue),
-        Revision.distinct(:author).select_map(:author),
-        Report.distinct(:author).select_map(:author),
-        Attachment.distinct(:author).select_map(:author)
+        Ticket.distinct.select(:reporter).select_map(:reporter),
+        Ticket.distinct.select(:owner).select_map(:owner),
+        TicketChange.distinct.select(:author).select_map(:author),
+        TicketChange.distinct.select(:newvalue).where(field: "reporter").select_map(:newvalue),
+        Revision.distinct.select(:author).select_map(:author),
+        Report.distinct.select(:author).select_map(:author),
+        Attachment.distinct.select(:author).select_map(:author)
       ].flatten.uniq.compact
 
       milestones = {}
       Milestone.each { |r| milestones[r.name] = r.to_hash }
 
-      types       = Ticket.distinct(:type).select_map(:type).compact
-      components  = Ticket.distinct(:component).select_map(:component).compact
-      resolutions = Ticket.distinct(:resolution).select_map(:resolution).compact
-      severity    = Ticket.distinct(:severity).select_map(:severity).compact
-      priorities  = Ticket.distinct(:priority).select_map(:priority).compact
-      tracstates  = Ticket.distinct(:status).select_map(:status).compact
+      types       = Ticket.distinct.select(:type).select_map(:type).compact
+      components  = Ticket.distinct.select(:component).select_map(:component).compact
+      resolutions = Ticket.distinct.select(:resolution).select_map(:resolution).compact
+      severity    = Ticket.distinct.select(:severity).select_map(:severity).compact
+      priorities  = Ticket.distinct.select(:priority).select_map(:priority).compact
+      tracstates  = Ticket.distinct.select(:status).select_map(:status).compact
 
       {
         "users" => Utilities.make_hash("", users),
--- lib/tractive/models/subticket.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Tractive
-  class Subticket < Sequel::Model(:subtickets)
-  end
-end

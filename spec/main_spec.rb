8eb3b53d66d2980e55e2254d6e4cfd92ba5ae085
hassanakbar <hassanakbar2@gmail.com>
2021-09-23 16:35:46 +0500

    main script test cases added

---
+++ spec/main_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe Tractive::Main do
+  it "should Exit if config file is not present" do
+    expect { Tractive::Main.new(config: "invalid-file") }.to raise_error(SystemExit)
+  end
+
+  it "should not Exit if config file is present" do
+    expect { Tractive::Main.new(options_for_main) }.not_to raise_error
+  end
+
+  it "should Exit if filter params are missing" do
+    expect { Tractive::Main.new(options_for_main(filter: true)) }.to raise_error(SystemExit)
+  end
+
+  it "should not Exit if all filter params are present" do
+    expect do
+      Tractive::Main.new(
+        options_for_main(
+          filter: true,
+          columnname: "priority",
+          operator: "=",
+          columnvalue: "medium"
+        )
+      )
+    end.not_to raise_error
+  end
+
+  def options_for_main(options = {})
+    options.merge(
+      config: "spec/files/test.config.yaml"
+    )
+  end
+end

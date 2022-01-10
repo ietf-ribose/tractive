327e2bfb5964335b842c4c878a1756423dee548a
hassanakbar <hassanakbar2@gmail.com>
2022-01-10 20:03:31 +0500

    graph_ql client added

---
+++ lib/tractive/github_api/graph_ql_client.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative "graph_ql_client/issues"
+
+require "graphql/client"
+require "graphql/client/http"
+
+# Service to perform github actions
+module GithubApi
+  class GraphQlClient
+    include GithubApi::GraphQlClient::Issues
+
+    HttpAdapter = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
+      attr_writer :token
+
+      def headers(_context)
+        {
+          "Authorization" => "bearer #{@token}"
+        }
+      end
+    end
+
+    def self.add_constants(token)
+      HttpAdapter.token = token
+
+      GithubApi::GraphQlClient.const_set("Schema", GraphQL::Client.load_schema(HttpAdapter))
+      GithubApi::GraphQlClient.const_set("Client", GraphQL::Client.new(schema: Schema, execute: HttpAdapter))
+      GithubApi::GraphQlClient.const_set("DeleteIssueQuery", Client.parse(DELETE_ISSUE_QUERY))
+    end
+  end
+end
---
+++ lib/tractive/github_api/graph_ql_client/issues.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module GithubApi
+  class GraphQlClient
+    # Methods for the Issues API
+    module Issues
+      DELETE_ISSUE_QUERY = <<~QUERY
+        mutation ($input: DeleteIssueInput!) {
+          deleteIssue(input: $input) {
+            repository {
+              name
+              url
+            }
+          }
+        }
+      QUERY
+
+      def delete_issue(issue_id)
+        variables = {
+          "input" => {
+            "issueId" => issue_id
+          }
+        }
+
+        GraphQlClient.query(DeleteIssueQuery, variables: variables)
+      end
+    end
+  end
+end

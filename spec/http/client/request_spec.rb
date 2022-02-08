2875e274f0a6e01c269497c30ba260812bdbcc92
hassanakbar <hassanakbar2@gmail.com>
2022-02-08 15:24:15 +0500

    fixed test cases

--- spec/http/client/request_spec.rb
+++ spec/http/client/request_spec.rb
@@ -23,10 +23,12 @@ RSpec.describe Http::Client::Request do
         call_count == 1 ? raise(RestClient::Forbidden) : { status: 200 }
       end
 
-      retry_text = "\rRate Limit Exceeded, Will retry in 0 min 1 sec"
-      expect { Http::Client::Request.execute(method: :get, max_retries: 1) }
-        .to output(retry_text)
-        .to_stdout
+      retry_text = "Rate Limit Exceeded, Will retry in 0 min 1 sec"
+      allow($logger).to receive(:info).with(retry_text)
+
+      Http::Client::Request.execute(method: :get, max_retries: 1)
+
+      expect($logger).to have_received(:info).with(retry_text).once
     end
 
     it "should `raise` exception if it occours after max retries" do
